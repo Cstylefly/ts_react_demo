61667df18135029adef0be1e4262e069132e258e
tangc <169713467@qq.com>
2021-08-09 15:14:27 +0800

    App修改

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.less';
-import {Button} from 'antd'
+import _ from 'lodash'
+import {Menu, MenuTheme} from 'antd'
 
-function App() {
+interface menuType{
+    name:string,
+    id:string,
+    children?:menuType[]
+}
+
+const meunList:menuType[] = [
+  {
+    name:'首页',
+    id:'sy'
+  },
+  {
+    name:'管理员',
+    id:'gly'
+  },
+  {
+    name:'普通用户',
+    id:'ptyh',
+    children:[
+      {
+        name:'张三',
+        id:'ptyh-1'
+      },
+      {
+        name:'李四',
+        id:'ptyh-2'
+      }
+    ]
+  }
+]
+
+const App = () => {
+  const [menuTheme,setTheme] = useState('light' as MenuTheme)
+
+  useEffect(() => {
+    setTheme('dark')
+  },[menuTheme])
+
   return (
     <div className="App">
-      <Button type="primary">hhh</Button>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <Menu 
+        mode={'horizontal'}
+        theme={menuTheme}
+        defaultSelectedKeys={[meunList[0].id || '']}
+      >
+        {
+          meunList.map((it,index) => {
+            if(_.isUndefined(it.children)){
+              return (
+                <Menu.Item key={it.id || index}>{it.name || ''}</Menu.Item>
+              )
+            }else{
+              return (
+                <Menu.SubMenu key={it.id || index} title={it.name || ''}>
+                  {
+                    (it.children || []).map((its,indexs) => {
+                      return (
+                        <Menu.Item key={its.id || indexs}>
+                          {its.name || ''}
+                        </Menu.Item>
+                      )
+                    })
+                  }
+                </Menu.SubMenu>
+              )
+            }
+          })
+        }
+      </Menu>
     </div>
   );
 }
